b92146debaed9690cc761ed2313208f958bae894
Nathan Merritt <gnm@the-merritts.net>
2015-10-01 19:48:38 -0400

    mock style unit tests

---
+++ tests/mocks.py
@@ -0,0 +1,32 @@
+class MockLogger(object):
+    def received(self, line):
+        self.last_received = line
+
+    def sent(self, line):
+        self.last_sent = line
+
+
+class MockFactory(object):
+    def __init__(self):
+        self.bot = MockBot()
+        self.logger = MockLogger()
+
+    def on_finish(self):
+        pass
+
+
+class MockProcess(object):
+    def kill(self):
+        self.killed = True
+
+    def tell(self, line):
+        self.last_told = line
+
+
+class MockBot(object):
+    def register(self, protocol):
+        self.registered = True
+
+    def login(self):
+        self.logged_in = True
+        return "login"
---
+++ tests/test_protocol.py
@@ -0,0 +1,36 @@
+import unittest
+from pipes.protocols import PokerProtocol
+from mocks import MockFactory, MockProcess
+
+
+class PokerProtocolTest(unittest.TestCase):
+    def send_line(self, line):
+        self.last_line = line
+
+    def setUp(self):
+        self.protocol = PokerProtocol()
+        self.protocol.sendLine = self.send_line
+        self.protocol.process = MockProcess()
+        self.protocol.factory = MockFactory()
+
+    def test_connection_made(self):
+        self.protocol.connectionMade()
+        self.assertTrue(self.protocol.factory.bot.logged_in)
+        self.assertTrue(self.protocol.factory.bot.registered)
+        self.assertEquals(self.last_line, "login")
+
+    def test_connection_lost(self):
+        self.protocol.connectionLost("explode")
+        self.assertTrue(self.protocol.process.killed)
+
+    def test_line_received(self):
+        p = self.protocol
+        p.lineReceived("a line")
+        self.assertEquals("a line", p.process.last_told)
+        self.assertEquals("a line", p.factory.logger.last_received)
+
+    def test_tell_server(self):
+        p = self.protocol
+        p.tell_server("hello")
+        self.assertEquals("hello", self.last_line)
+        self.assertEquals("hello", p.factory.logger.last_sent)
